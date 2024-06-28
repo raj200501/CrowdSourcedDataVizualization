9bbd34a760c7afaa35106b9807406d77d567bedb
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-28 01:35:54 -0700

    Create CollaborationPage.js

---
+++ frontend/src/pages/CollaborationPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RealTimeCollaboration from '../components/RealTimeCollaboration';
+
+function CollaborationPage() {
+  return (
+    <div>
+      <h1>Collaboration</h1>
+      <RealTimeCollaboration />
+    </div>
+  );
+}
+
+export default CollaborationPage;
