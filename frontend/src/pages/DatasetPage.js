ae96b18ea79d78666f5a8eaddc12ccbc4deaa88c
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-28 01:35:24 -0700

    Create DatasetPage.js

---
+++ frontend/src/pages/DatasetPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataCleaning from '../components/DataCleaning';
+
+function DatasetPage() {
+  return (
+    <div>
+      <h1>Datasets</h1>
+      <DataCleaning />
+    </div>
+  );
+}
+
+export default DatasetPage;
