43a19b89923141d0f50a30246da37666e61a69cd
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-28 01:35:38 -0700

    Create VisualizationPage.js

---
+++ frontend/src/pages/VisualizationPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataVisualization from '../components/DataVisualization';
+
+function VisualizationPage() {
+  return (
+    <div>
+      <h1>Visualizations</h1>
+      <DataVisualization />
+    </div>
+  );
+}
+
+export default VisualizationPage;
