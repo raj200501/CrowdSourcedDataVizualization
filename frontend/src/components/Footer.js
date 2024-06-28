aebac0c4f76cbeb5ae4e664f3c20790aca9f19de
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-28 01:33:49 -0700

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Crowdsourced Data Visualization Platform</p>
+    </footer>
+  );
+}
+
+export default Footer;
